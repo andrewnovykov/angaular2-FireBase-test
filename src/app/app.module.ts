a95b9a37ec7fb673a78eec9b77183eaa321ae348
papapin <papapin777@gmail.com>
2016-11-12 18:39:01 +0200

    complit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from 'angularfire2';
+import { firebaseConfig } from '../environments/firebase.config';
+
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +16,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp( firebaseConfig )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: " ",
+    authDomain: "angular-2-test-95796.firebaseapp.com",
+    databaseURL: "https://angular-2-test-95796.firebaseio.com",
+    storageBucket: "angular-2-test-95796.appspot.com"
+
+}
